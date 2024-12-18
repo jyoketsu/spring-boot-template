29cc846d80d7fe5b04b35d5935fbbbcf6b317320
xujie <xujie@titanai.cc>
2024-12-18 11:09:09 +0800

    feat: RESTful Web Service

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,14 +1,22 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class HelloController {
 
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
 	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
+	public Hello hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Hello(counter.incrementAndGet(), String.format(template, name));
 	}
 }
